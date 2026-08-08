cf861733bfa233e5197f200992ed8bc444087b28
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2026-08-08 22:47:56 +0300

    enhance isAllowedRoles

--- src/components/auth/isAllowedRoles.ts
+++ src/components/auth/isAllowedRoles.ts
@@ -1,27 +1,24 @@
 import { redirect } from "next/navigation"
 import { getSession } from "./getSession"
 import { Role } from "@/generated/prisma/enums"
+import prisma from "@/lib/prisma"
 
 export const isAllowedRoles = async (allowedRoles: Role[]) => {
   const session = await getSession()
-
-  // 1. إذا لم يكن هناك جلسة تسجيل دخول أصلاً
-  if (!session?.user) {
-    redirect("/login") // أو التوجيه للرئيسية "/"
-  }
-
   const superAdminEmail = process.env.SUPER_ADMIN
 
-  // 2. السماح الفوري للـ Super Admin
-  if (superAdminEmail && session.user.email === superAdminEmail) {
-    return session
-  }
-
-  // 3. التحقق من مطابقة الدور الحالي للمستخدم للأدوار المسموحة
-  const userRole = session.user.role as Role
-  if (!allowedRoles.includes(userRole)) {
+  if (!session?.user) {
     redirect("/login")
   }
 
-  return session
+  if (superAdminEmail && session.user.email === superAdminEmail) {
+    return
+  }
+
+  const dbUser = await prisma.user.findUniqueOrThrow({ where: { email: session.user.email } })
+  const dbUserRole = dbUser?.role ?? "user"
+  if (!allowedRoles.includes(dbUserRole)) {
+    redirect("/login")
+  }
+  return
 }
